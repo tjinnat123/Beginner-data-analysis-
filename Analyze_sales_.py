7dc8dacd079c243e22af4f960ec2953bb24597fa
tjinnat123 <tjinnat123@gmail.com>
2025-11-25 12:21:14 -0600

    Added analyze_sales.py
    This program reads a CSV file containing sales data and calculates total and average sales.

---
+++ Analyze_sales_.py
@@ -0,0 +1,14 @@
+# analyze_sales.py
+# this program reads a csv sales file and calculates total and average sales
+import pandas as pd # import the pandas library for data analysis 
+# read the csv file
+data = pd.read_csv("sales_dara.csv")
+#print the first 5 rows
+print( "sales data oreview:")
+print(data.head())
+# calculate total sales
+total_sales = data["sales"].sum()
+print("\ntotal sales:",total sales)
+# calcukate average sales
+average_sales = data["sales"].mean()
+print("average sales:",average_sales)
